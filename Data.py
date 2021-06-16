28eca2d8df0d1a9ea30ca010bc04292417a25680
wojtek5739g <45803830+wojtek5739g@users.noreply.github.com>
2021-06-16 03:23:50 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -112,7 +112,7 @@ class Data:
                 Przejmujesz [{pole}] po przekręcie finansowym dokonanym przez waszego 
                 księgowego, który przekupił pracownika Urzędu Skarbowego, aby ten przymknął oko 
                 na wasze poczynania. Uważaj na rozwścieczonych bandytów Wołominu, czyhających w okolicznych dzielnicach.""",
-                """Poszukując Anastazji, Pershing wysyła Cię na zwiady w okolice stadionu, w okolicy kótrego po raz ostatni
+                """Poszukując Anastazji, Pershing wysyła Cię na zwiady w okolice stadionu, w okolicy którego po raz ostatni
                 wychwycono sygnał z jej komórki. Po córce szefa ani śladu, za to znaleźliście skład kontrabandy ukryty w piwnicy
                 opustoszałego bundyku nieopdal stadionu. Postanawiacie przejąć nieruchomość, opłacając skarbówkę.""",
                 """Inwestycje rządzą światem. Przekazujecie kilkadziesiąt tysięcy gotówki na rozkręcenie lombardu nieopodal {pole}.
@@ -140,7 +140,7 @@ class Data:
                 dość ciekawy obrót, gdy zyskujecie potężną kartę przetargową w postaci dowodów defraudacji pieniężnej przez biznesmena, kierującego wiekszością 
                 okolicznych mini-przedsiębiorstw. Od tej pory ściągacie z nich haracz.""",
                 """{pole} jest idealnym miejscem pod wybudowanie nowej restauracji "Srogi Okoń", w podziemiach której razem z zaufanym doradcą podatkowym Miry
-                uruchamiacie kasyno. Pershing zjawaia się tam razem z zarządem. Świetnie się bawicie.""",
+                uruchamiacie kasyno. Klepak zjawaia się tam razem z zarządem. Świetnie się bawicie.""",
                 """Po ciężkim dniu w pracy razem ze swoimi przyjaciółmi przesiadujecie w barze, gdzie barman sprzedaje wam cynk o nielegalnej
                 licytacji budynku na {pole} lansowanej przez skorumpowanych służbistów Urzędu Ochrony Państwa. Nie możesz przepuścić tej okazji. 
                 Dzwonisz do Klepaka, który daje Ci aprobatę na przejęcie licytacji ze specjalnie przydzielonym zespołem interwencyjnym.""",
@@ -191,13 +191,19 @@ class Data:
                 z Pruszkowa wyprawiają Ci huczne przyjęcie urodzinowe na jachcie na Wiśle. Otrzymujesz pokaźne podarki. Pobierz M250.""",
                 """Podczas wymiany zakładników w Ożarowie Mazowieckim nieopodal willi Pershinga dochodzi do strzelaniny, w wyniku której zostajesz postrzelony
                 w nogę. Trafiasz do szpitala. Aby uniknąć alarmowania policji o zajściu, płacisz dodatkowo lekarzowi M150.""",
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """
+                """Jeremiasz "Baranina" Barański prosi Cię o przysługę. Dziwisz się, gdyż jest ekspertem od załatwiania wszelkich spraw. Niemniej jednak
+                Centrum Bankowo-Finansowego, aby dostarczyć dokumenty, o co prosił cię "Baranina". Idź na Aleje Jerozolimskie. Jeżeli przejdziesz przez START,
+                pobierz M250.""",
+                """Pershing na zgromadzeniu zarządu postanawia odrestaurować część posiadanych nieruchomości. Każdy z was płaci M25 do wspólnego majątku 
+                rewaloryzacyjnego.""",
+                """Dzisiaj są imieniny drugiej córki szefa. Pershing każe Ci pójść z nią do kina "Wisła". Idź na plac Wilsona. Płacisz M5.""",
+                """W niewyjaśnionych okolicznych ginie Twój bezdzietny wuj, emerytowany członek mafii wołomińskiej, który jednak postanowił Tobie, najstarszemu
+                bratankowi przekazać w spadku swój majątek. Pobierz M400.""",
+                """Będąc w wirze nieustannych walk między najpotężniejszymi gangami w Warszawie, zaskakuje Cię wieść o niespłaconych rachunkach za prąd i wodę.
+                Odłożenie rachunków sprawia, że musisz dopłacić dodatkowe M45.""",
+                """W warszawskiej restauracji 'Gamma' dochodzi do masakry, w której ginie czterech pruszkowskich gangsterów z rąk zamaskowanych napastników.
+                O mało nie giniesz w trakcie. Tracisz jedno z pól i M25.""",
+                """W kasynie Miry Orłow wraz z jednym z współpracowników pod przykrywką obcokrajowców oszukujecie podczas gry w jednorękiego bandytę. Zyskujesz M50."""
 
             ],
             'F': ["""Obrabowałeś bank na przedmieściach. Zyskujesz dla swojego gangu M200.""",
@@ -209,22 +215,39 @@ class Data:
                 do tradycji mafijnych waszego ugrupowania, musisz zapłacić 115M jako zapomogę dla dawnego szefa.""",
                 """Mira, jako miłośniczka odradzającej się zachodniej kultury, organizuje przepastny 
                 wybieg mody w warszawskim "Domu Towarowym Braci Jabłkowskich". Kierownik centrum handlowego zatrudnia Cię jako bramkarza
-                na wydarzeniu. Zyskujesz 25M. """,
+                na wydarzeniu. Zyskujesz 40M. """,
                 """Przejdź na start. Wspieraj gang na co raz to nowsze sposoby.""",
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """,
-                """ """
+                """Klepak w porozumieniu z Mirą organizują zlot kilku mniejszych ugrupowań mafijnych z okolic Warszawy, aby dokonać ewentualnej fuzji. Wydarzenie
+                jest huczne, poprzedzone balem, kolacją i występem kilku raperów. Udajesz się na Ulicę Marszałkowską.""",
+                """Zaufany kontakt Mariana Klepackiego Andrzej ps. 'Kikir', z którym wcześniej Wołomin dokonywał wielu utargów, ma zamiar przylecieć samolotem 
+                z Gdańska do Warszawy z gotówką i kilkoma gangsterami, co ma pomóc w zaognionej walce z Pruszkowem. Idź na najbliższe lotnisko. Zapłać M10.""",
+                """Niespodziewanie dochodzi do ataku gangsterów z ugrupowania ząbkowsko-praskiego, który to mały odłam mafii warszawskiej wykorzystuje zaistniałą
+                sytuację. Tracisz jedno pole.""",
+                """Marian Klepak organizuje imprezę w podwarszawskim klubie "Acapulco". Jest to jedna z większych imprez tuż po upadku komunistycznego rządu. 
+                Alkohol leje się strumieniami, podobnie jak pieniądze. Każdy gracz zyskuje M150.""",
+                """Mira Orłow, kontynuując swoją osobistą wendetę (której genezą z nikim nie chce się podzielić) na byłym mężu Andrzeju 'Pershingu' Kolikowskim, 
+                organizuje najazd na Państwowe Zakłady Tele- i Radiotechniczne, w którym uczestniczysz. Wie, że dokładnie tam jej były mąż ukrył pokaźne ilości gotówki
+                jeszcze z czasów sprzed upadku komunizmu. Doskonale orientuje się w sytuacji, że urząd ten jest ściśle chroniony przez oddział policyjny, 
+                który nie przepuści nikogo z gangu pruszkowskiego przez próg, za to ją, mającą silne wpływy w Sądzie Najwyższym, owszem. Udaj się na ulicę Grochowską.""",
+                """Andrzej 'Kikir' przesyła pozdrowienia Klepakowi wraz z solidnymi porcjami kontrabandy i gotówki w zaplombownych skrzynkach. Zyskujesz M50
+                jako premię za dobre sprawowanie. """,
+                """Szpicel Miry Orłow z Agencji Bezpieczeństwa Wewnętrznego dowiaduje się, że dojdzie do próby zlikwidowania "Baniaka" – wielokrotnie utrudniającego śledztwa
+                przeciwko Wołominowi sekretarza Centralnego Biura Śledczego, kuzyna Miry. Rozkazuje Ci i kilku gangsterom sprawować nad nim ścisłą ochronę w najbliższych
+                dniach. Udajesz się na Nowy Świat.""",
+                """W ramach zakrojonej na wielką skalę akcji Krajowej Administracji Skarbowej, mającej na celu wychwycenie aktów korupcji i defraudacji, Klepak rozkazuje
+                Ci pomóc w usuwaniu wszelkich śladów niedogodności finansowych w jednej z siedzib gangu. Cofasz się o cztery pola.""",
+                """Na granicy ulicy Ząbkowskiej i Kalinowskije zauważasz, że śledzi Cię od dłuższego czasu tajemniczy czarny pojazd z zadymionymi szybami. Nadrabiasz
+                więc drogi jadąc do willi Klepaka. Cofnij się o trzy pola.""",
+                """Na spotkaniu zarządu w hotelu jednego z kuzynów Klepaka okazuje się, że kuzyn ten jest podwójnym agentem. CBA wkracza do akcji, aresztując razem z policją
+                wielu gangsterów wołomińskich i przechwycając gotówkę z nielegalnego obrotu. Tracisz M50.""",
+                """Anastazja Kolikowska, córka Pershinga, ucieka z niewoli po otrzymaniu pomocy ze strony jednego z gangsterów, któy jak się okazało, był podwójnym agentem. 
+                Mira rokazuje Ci znaleźć Anastazję za wszelką cenę i podpowiada, gdzie mogła uciec. Udaj się na Elektrownię. Jeśli przechodzisz przez START, nie pobieraj
+                dodatkowych pieniędzy""",
+                """Bez uzgodnienia z zarządem wołomińskim gangster Jeremiasz ps. "Tyburek" Tyburski atakuje i zabija policjanta, który aresztował jego brata. Wszyscy płacicie
+                M25 do wspólnego majątku gangu, żeby zamieść sprawę pod dywan.""",
+                """Ginie syn Mariana Klepaka w starciu z policją w jednym z podwarszawskich klubów. Jedziesz na jego pogrzeb. Udaj się na Aleje Ujazdowskie. """,
+                """Do Twojego domu wita człowiek podający się za agenta nieruchomości. Nie spodziewałeś\aś się nikogo takiego, więc po krótkiej rozmowie okazuje się, że było
+                to zwykłe nieporozumienie. 2 godziny po jego wyjściu orientujesz się, że z Twojej szuflady giną oszczędności. Tracisz M50."""
 
             ]
         }
